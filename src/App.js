df514e4e16c2de8fce42ee24491436ea2da72750
rennitbaby <work.kevin.ren@gmail.com>
2023-01-21 03:21:28 -0800

    first commit, skeleton setup

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div class="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
+    Hello World draftbuddy
+  </div>
   );
 }
 
---
+++ src/containers/DraftInput/container.jsx
@@ -0,0 +1,2 @@
+// going to be a nice looking form
+// composed of components that is styled at the component lvl
