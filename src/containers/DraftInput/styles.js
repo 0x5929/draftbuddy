454112ec052dee19c95ff8026169c60a150abb77
rennitbaby <work.kevin.ren@gmail.com>
2023-01-24 03:45:38 -0800

    switched up css classname orders for draftInput

--- src/containers/DraftInput/styles.js
+++ src/containers/DraftInput/styles.js
@@ -17,7 +17,7 @@ const useDraftInputSytles = () => {
     gridContainer: [
       'grid',
       'md:grid',
-      'grid-cols-1',
+      'grid-cols-1', 
       'md:grid-cols-1',
       'gap-6',
       'md:gap-6'
@@ -36,10 +36,10 @@ const useDraftInputSytles = () => {
     ],
 
     cancelBtn: [
-      'border-gray-300',
       'bg-white',
+      'hover:bg-gray-50',
+      'border-gray-300',
       'text-gray-700',
-      'hover:bg-gray-50'
     ],
 
     draftBtn: [
@@ -47,11 +47,11 @@ const useDraftInputSytles = () => {
       'inline-flex',
       'justify-center',
       'border-transparent',
-      'bg-indigo-600',
       'text-white',
 
+      'bg-indigo-600',
       'hover:bg-indigo-700',
-      
+
     ]
     
     
