fce54560ea6f9b7babaf8508bcf4bd2ff5f9b3a5
rennitbaby <work.kevin.ren@gmail.com>
2023-01-31 00:17:44 -0800

    algo done!! next is writing tests to display data and code the card displays

--- src/utils/algorithm.js
+++ src/utils/algorithm.js
@@ -13,10 +13,44 @@
  * 
  *  outputs: 
  *    1. array of objects with key of each position of an array of player objects (3 at each pick, each position)
- *    2. NOTE: output is under the assumption there are 15 rounds total, and 15 players per team
+ *    2. NOTE: output is under the assumption there are 15 rounds total, and 15 draftable players per team
  */
 
 function algorithm ({headCount, draftPick, data}) {
+  // https://stackoverflow.com/questions/8584902/get-the-closest-number-out-of-an-array
+  function closestValue(array, value) {
+    let result
+    let lastDelta;
+
+    array.some((item) => {
+        const delta = Math.abs(value - item.adp)
+        if (delta > lastDelta) {
+            return true
+        }
+        result = item
+        lastDelta = delta
+        return false
+    })
+    return result
+  }
+
+  // sort players by ADP value, higher gets pushed.
+  function adpSort(a, b)  {
+    let comp 
+    if (a.adp === b.adp) {
+      comp = 0
+    }
+    else if (a.adp < b.adp){
+      comp = -1
+    }
+    else if (b.adp < a.adp) {
+      comp = 1
+    }
+
+    return comp
+
+  }
+  
   function resolveOPNs() {
     // create base array
     const base = [...Array(parseInt(headCount, 10)).keys()].map(x => x + 1)
@@ -56,41 +90,45 @@ function algorithm ({headCount, draftPick, data}) {
 
   }
   function sortData(ODP) {
-
-    // sort players by ADP value, higher gets pushed.
-    const adpSort = (a, b) => {
-      let comp 
-      if (a.adp === b.adp) {
-        comp = 0
-      }
-      else if (a.adp < b.adp){
-        comp = -1
-      }
-      else if (b.adp < a.adp) {
-        comp = 1
-      }
-
-      return comp
-
-    }
-
     // sort data base on ODP, into what we can display in DraftOutput
     const { players } = data
-    const playerSorted = players.sort(adpSort)
+    const resultDraft = []
 
-    // look in small radius, for three, then apply rules to three. 
-    // then if not look in bigger (repeate above step with bigger radius, until three is found)
-    // const qbs = players.filter((el) => el.position === 'QB').sort(adpSort)
-    // const rbs = players.filter((el) => el.position === 'RB').sort(adpSort)
-    // const wrs = players.filter((el) => el.position === 'WR').sort(adpSort)
-    // const tes = players.filter((el) => el.position === 'TE').sort(adpSort)
+    // sort by pos
+    const qbs = players.filter((el) => el.position === 'QB').sort(adpSort)
+    const rbs = players.filter((el) => el.position === 'RB').sort(adpSort)
+    const wrs = players.filter((el) => el.position === 'WR').sort(adpSort)
+    const tes = players.filter((el) => el.position === 'TE').sort(adpSort)
+  
+    for (let i = 0; i < ODP.length; i++) {
+      // each round of draft
 
-    // console.log('data.players: ', data.players)
-    // console.log('qbs: ', qbs)
-    // console.log('rbs: ', rbs)
-    // console.log('wrs: ', wrs)
-    // console.log('tes: ', tes)
-    return data
+      
+      // for each ODP, grab each pos closest
+      const clstQB = closestValue(qbs, ODP[i].ODP)
+      const clstRB = closestValue(rbs, ODP[i].ODP)
+      const clstWR = closestValue(wrs, ODP[i].ODP)
+      const clstTE = closestValue(tes, ODP[i].ODP)
+
+      
+      // see what index closest is, and take its following three for each ODP
+      const qbIndx = qbs.findIndex((element) => element.player_id === clstQB.player_id)
+      const rbIndx = rbs.findIndex((element) => element.player_id === clstRB.player_id)
+      const wrIndx = wrs.findIndex((element) => element.player_id === clstWR.player_id)
+      const teIndx = tes.findIndex((element) => element.player_id === clstTE.player_id)
+
+      resultDraft.push({
+        meta: ODP[i],
+        players: {
+          qb: [qbs[qbIndx], qbs[qbIndx + 1], qbs[qbIndx + 2]],
+          rb: [rbs[rbIndx], rbs[rbIndx + 1], rbs[rbIndx + 2]],
+          wr: [wrs[wrIndx], wrs[wrIndx + 1], wrs[wrIndx + 2]],
+          te: [tes[teIndx], tes[teIndx + 1], tes[teIndx + 2]],
+        }
+      })
+    }
+    console.log('resultDraft: ', resultDraft)
+    return resultDraft
   }
 
   
